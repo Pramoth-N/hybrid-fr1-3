097bce907581e2ff5fa85fe27dccbfbf8ad5f641
neojarvis <techadmin@iamneo.ai>
2025-08-21 09:38:09 +0000

    Auto-save-commit-8/21/2025--3:11:12-PM

---
+++ Project/src/test/java/uistore/IhclUI.java
@@ -0,0 +1,8 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class IhclUI {
+    public static By company = By.xpath("//*[@id='__next']/main/div[1]/div/div[2]/div/div[2]/span");
+    public static By aboutUs = By.
+}
